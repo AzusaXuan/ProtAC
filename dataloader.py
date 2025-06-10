363f546cc3bcadcf08536d8df084c075a52f8d0b
Zixuan Jiang <zjiang597@connect.hkust-gz.edu.cn>
2025-06-10 18:44:40 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -93,13 +93,13 @@ class Dataset_pretrain_new(data.Dataset):
         self.kws = 'kws'
 
         if epoch == 0:
-            self.en_path = f'/hpc2hdd/home/lzhang819/zixuanjiang/datasets/{self.dataset}/{self.dataset}_new_split_sample_merged.hdf5'  # merged; untokenized
+            self.en_path = f'/datasets/{self.dataset}/{self.dataset}.hdf5'
         else:
             self.en_path = captioned_seq_sav_dir
         with h5py.File(self.en_path, 'r') as f:
             self.en_train_count = f[self.seqs].shape[0]
 
-        self.sw_path = "/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swissprot/swissprot_double.hdf5"
+        self.sw_path = "/datasets/swissprot/swissprot.hdf5"
         with h5py.File(self.sw_path, 'r') as f:
             self.sw_train_count = f[self.seqs].shape[0]
 
@@ -145,13 +145,13 @@ class Dataset_pretrain_untokenized(data.Dataset):
         self.truncation_seq_length = truncation_seq_length
 
         if epoch == 0:
-            self.en_path = f'/hpc2hdd/home/lzhang819/zixuanjiang/datasets/{self.dataset}/{self.dataset}_new_split_sample_untokenized.hdf5'  # merged; untokenized
+            self.en_path = f'/datasets/{self.dataset}/{self.dataset}.hdf5'
         else:
             self.en_path = captioned_seq_sav_dir
         with h5py.File(self.en_path, 'r') as f:
             self.en_train_count = f[self.seqs].shape[0]
 
-        self.sw_path = "/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swissprot/swissprot_train_untokenized_new.hdf5"
+        self.sw_path = "/datasets/swissprot/swissprot_train_untokenized_new.hdf5"
         with h5py.File(self.sw_path, 'r') as f:
             self.sw_train_count = f[self.seqs].shape[0]
 
@@ -215,7 +215,7 @@ class Dataset_caption(data.Dataset):
         self.kws = 'kws'
 
         if epoch == 0:
-            self.en_path = f'/hpc2hdd/home/lzhang819/zixuanjiang/datasets/{self.dataset}/{self.dataset}_new_split_sample_merged.hdf5'
+            self.en_path = f/datasets/{self.dataset}/{self.dataset}.hdf5'
         else:
             self.en_path = captioned_seq_sav_dir
         with h5py.File(self.en_path, 'r') as f:
@@ -246,7 +246,7 @@ class Dataset_swiss_random(data.Dataset):
     def __init__(self, num_annotation, split="train"):
         super(Dataset_swiss_random, self).__init__()
 
-        self.path = f"/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swissprot/swissprot_double.hdf5"
+        self.path = f"/datasets/swissprot/swissprot.hdf5"
         # self.cnt = len(os.listdir(self.path))
         # print("swiss cnt: ", self.cnt)
         self.seqs = 'seqs'
@@ -284,7 +284,7 @@ class Dataset_finetune_untokenized(data.Dataset):
         self.truncation_seq_length = truncation_seq_length
         self.seq_len = seq_len
         self.n_annotation = num_annotation
-        self.sw_path = f"/hpc2hdd/home/zjiang597/datasets/swissprot/swissprot_train_untokenized_new.hdf5"
+        self.sw_path = f/datasets/swissprot/swissprot_train_untokenized_new.hdf5"
         with h5py.File(self.sw_path, 'r') as f:
             self.sw_train_count = f[self.seqs].shape[0]
         print("sw_train_count", self.sw_train_count)
@@ -330,7 +330,7 @@ class Dataset_swiss_random_test(data.Dataset):
     def __init__(self):
         super(Dataset_swiss_random_test, self).__init__()
 
-        self.path = f"/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swissprot/test"
+        self.path = f"/datasets/swissprot/test"
         self.cnt = len(os.listdir(self.path))
 
     def __len__(self):
@@ -360,7 +360,7 @@ class Dataset_swiss_random_test_untokenized(data.Dataset):
         self.truncation_seq_length = truncation_seq_length
         self.seq_len = seq_len
         self.n_annotation = num_annotation
-        self.sw_path = f"/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swissprot/swissprot_test_untokenized_new.hdf5"
+        self.sw_path = f"/datasets/swissprot/swissprot_test_untokenized_new.hdf5"
         with h5py.File(self.sw_path, 'r') as f:
             self.sw_test_count = f[self.seqs].shape[0]
         print("sw_test_count", self.sw_test_count)
@@ -409,7 +409,7 @@ class Dataset_swiss_testset_kw(data.Dataset):
         self.alphabet = alphabet
         self.truncation_seq_length = truncation_seq_length
         self.seq_len = seq_len
-        self.path = f"/hpc2hdd/home/zjiang597/datasets/swiss_test_kw_773_11/{split}"
+        self.path = f"/datasets/swiss_test_kw_773_11/{split}"
         self.cnt = len(os.listdir(self.path))
         self.original_letter_to_num = {'C': 4, 'D': 3, 'S': 15, 'Q': 5, 'K': 11, 'I': 9,
                                        'P': 14, 'T': 16, 'F': 13, 'A': 0, 'G': 7, 'H': 8,
@@ -466,7 +466,7 @@ class Dataset_swiss_testset_kw_pb(data.Dataset):
         super(Dataset_swiss_testset_kw_pb, self).__init__()
         self.truncation_seq_length = truncation_seq_length
         self.seq_len = seq_len
-        self.path = f"/hpc2hdd/home/lzhang819/zixuanjiang/datasets/swiss_test_kw_773_11/{split}_new"
+        self.path = f"/datasets/swiss_test_kw_773_11/{split}_new"
         self.cnt = len(os.listdir(self.path))
         self.original_letter_to_num = {'C': 4, 'D': 3, 'S': 15, 'Q': 5, 'K': 11, 'I': 9,
                                        'P': 14, 'T': 16, 'F': 13, 'A': 0, 'G': 7, 'H': 8,
@@ -507,7 +507,7 @@ class Dataset_uniref50_caption_new(data.Dataset):
         self.seq_len = seq_len
         self.n_annotation = num_annotation
         if epoch == 0:
-            self.en_path = f'/hpc2hdd/home/lzhang819/zixuanjiang/datasets/{self.dataset}/{self.dataset}_new_split_sample_untokenized.hdf5'  # merged; untokenized
+            self.en_path = f'/datasets/{self.dataset}/{self.dataset}.hdf5'
         else:
             self.en_path = captioned_seq_sav_dir
         with h5py.File(self.en_path, 'r') as f:
@@ -531,67 +531,3 @@ class Dataset_uniref50_caption_new(data.Dataset):
         seq_lens = len(seqs)
         return torch.tensor(kws, dtype=torch.float), torch.tensor(seq_lens, dtype=torch.int), torch.tensor(
             inds, dtype=torch.int), torch.tensor(idx, dtype=torch.int), prot_ids, seqs
-
-
-if __name__ == '__main__':
-    model_esm2, alphabet = esm.pretrained.esm2_t6_8M_UR50D()
-    model_esm2.eval()  # disables dropout for deterministic results
-
-    pretrain_dataset = Dataset_uniref50_caption_new(alphabet=alphabet)
-    world_size = utils.get_world_size()
-    rank = utils.get_rank()
-    print(world_size, rank)
-    # sys.exit()
-    pretrain_sampler = create_sampler(pretrain_dataset, True, world_size, rank)
-    batch_size = 32
-
-    pretrain_dataloader = create_caption_loader(pretrain_dataset, pretrain_sampler, batch_size, 8)
-    truncation_seq_length = 512
-    for i, batch in enumerate(pretrain_dataloader):
-        tensor_batch, prot_ids, seq_str_list = batch
-        input_anno, input_seqlen, inds, idx = tensor_batch
-        print(prot_ids, seq_str_list)
-
-        seq_encoded_list = [alphabet.encode(seq_str) for seq_str in seq_str_list]
-        if truncation_seq_length:
-            seq_encoded_list = [seq_str[:truncation_seq_length] for seq_str in seq_encoded_list]
-        max_len = max(len(seq_encoded) for seq_encoded in seq_encoded_list)
-        tokens = torch.empty(
-            (
-                batch_size,
-                max_len + int(alphabet.prepend_bos) + int(alphabet.append_eos),
-            ),
-            dtype=torch.int64,
-        )
-        tokens.fill_(alphabet.padding_idx)
-
-        for i, seq_encoded in enumerate(seq_encoded_list):
-            if alphabet.prepend_bos:
-                tokens[i, 0] = alphabet.cls_idx
-            seq = torch.tensor(seq_encoded, dtype=torch.int64)
-            tokens[
-            i,
-            int(alphabet.prepend_bos): len(seq_encoded)
-                                       + int(alphabet.prepend_bos),
-            ] = seq
-            if alphabet.append_eos:
-                tokens[i, len(seq_encoded) + int(alphabet.prepend_bos)] = alphabet.eos_idx
-        print(tokens, tokens.shape)
-        sys.exit()
-        print(input_seqlen)
-        print(input_seq.shape, input_seqlen.shape)
-        with torch.no_grad():
-            results = model_esm2(input_seq, repr_layers=[33], return_contacts=False)
-        token_representations = results["representations"][33]
-        seq_logits = results["logits"].argmax(-1)
-        range_tensor = torch.arange(0, seq_logits.shape[1]).expand(seq_logits.shape[0], -1)
-
-        # 检查每个元素的索引是否小于b中对应的值
-        # 这将创建一个布尔掩码，其中True表示索引小于b[i]的元素
-        mask = range_tensor <= input_seqlen.unsqueeze(1)
-        mask[:, 0] = False
-        # 应用掩码到a，然后计算非零元素的数量
-        diff = seq_logits - input_seq
-        cnt = torch.count_nonzero(diff * mask)
-        print(cnt / input_seqlen.sum())
-        sys.exit()
